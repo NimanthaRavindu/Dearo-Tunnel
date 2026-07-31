5218416ac20c65cf59a998f0e969730ae69d76fa
Nimantha Ravindu <nimantharavindu713@gmail.com>
2026-07-31 11:32:21 +0530

    Describe your changes

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ export const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'apextunnel' || 'railway',
+    database: process.env.DB_NAME || 'apextunnel',
     waitForConnections:true,
     connectionLimit:10,
     queueLimit:0
